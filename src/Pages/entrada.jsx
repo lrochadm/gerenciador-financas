9ed2e1d6cb0e984085657802a10126da8666bdc0
Lucas Rocha <rocha.lucas0606@gmail.com>
2023-07-31 17:38:13 -0300

    Listas adicionadas, e estilização

--- src/Pages/entrada.jsx
+++ src/Pages/entrada.jsx
@@ -5,7 +5,40 @@ function Entrada() {
     <div className="page-container">
       <div className="content-title">
         <h2>Entradas</h2>
-        <button>Adicionar Entrada</button>
+        <button type="submit">Adicionar Entrada</button>
+      </div>
+      <div className="list-entrada">
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Data</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Salário</td>
+              <td>05/07</td>
+              <td>3.000,00</td>
+            </tr>
+            <tr>
+              <td>Salário</td>
+              <td>05/07</td>
+              <td>3.000,00</td>
+            </tr>
+            <tr>
+              <td>Salário</td>
+              <td>05/07</td>
+              <td>3.000,00</td>
+            </tr>
+            <tr>
+              <td>Salário</td>
+              <td>05/07</td>
+              <td>3.000,00</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
--- src/Pages/saida.jsx
+++ src/Pages/saida.jsx
@@ -1,12 +1,47 @@
 import "./saida.css";
 
 function Saida() {
-  return <div className="page-container">
-    <div className="content-title">
+  return (
+    <div className="page-container">
+      <div className="content-title">
         <h2>Saídas</h2>
-        <button>Adicionar Saída</button>
+        <button type="submit">Adicionar Saída</button>
       </div>
-  </div>;
+      <div className="list-saida">
+        <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Data</th>
+              <th>Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tim</td>
+              <td>10/09</td>
+              <td>45,00</td>
+            </tr>
+            <tr>
+              <td>Tim</td>
+              <td>10/09</td>
+              <td>45,00</td>
+            </tr>
+            <tr>
+              <td>Tim</td>
+              <td>10/09</td>
+              <td>45,00</td>
+            </tr>
+            <tr>
+              <td>Tim</td>
+              <td>10/09</td>
+              <td>45,00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Saida;
